098746fea00ff5f4b7285582b6bc9a33b8ce1ab9
anchitsom <anchitaxom@gmail.com>
2017-11-13 21:00:30 +0800

    Merged xml files

--- app/src/main/java/inquilab/music_maker/MainActivity.java
+++ app/src/main/java/inquilab/music_maker/MainActivity.java
@@ -23,23 +23,56 @@ import java.io.InputStream;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-  //grbyhg
-  Button play;
+
+    Button play;
     Button record;
     Button save;
     SeekBar bpm;
     TextView bpmtext;
     Spinner minimenu;
-    boolean paused = true;
+    boolean isPlaying = false;
     boolean recorded=true;
 
     String[] menu={"Recently Played","Most Used","Songs","dummy"};
 
-
     private AudioTrack[] track = new AudioTrack[2];
     private InputStream[] stream = new InputStream[2];
     private byte[][] music = new byte[2][];
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navmenu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    private void play(){
 
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    doStuff(0, R.raw.guitarc4);
+                }
+            };
+            Thread t1 = new Thread(r);
+            t1.start();
+
+            long futureTime = System.currentTimeMillis() + 5000;
+            while (System.currentTimeMillis() < futureTime) {
+                synchronized (this) {
+                    try {
+                        wait(futureTime - System.currentTimeMillis());
+
+                    } catch (Exception e) {
+                    }
+                }
+            }
+            doStuff(1, R.raw.guitarc3);
+
+    }
+    private void pause(){
+        track[0].pause();
+        track[0].flush();
+        track[1].pause();
+        track[1].flush();
+    }
     private void doStuff(int stream_id, int resource_id) {
 
         setResource(stream_id, resource_id);
@@ -51,7 +84,7 @@ public class MainActivity extends AppCompatActivity {
             while((i = stream[stream_id].read(music[stream_id])) != -1)
             {
                 track[stream_id].write(music[stream_id], 0, i);
-                Log.d(getClass().getName(), "samples: " + Arrays.toString(music));
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -63,113 +96,16 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     private void setResource(int stream_id, int resource_id){
-//        try {
-//            stream[stream_id].close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
         stream[stream_id] = getResources().openRawResource(resource_id);
     }
-    boolean flag=true;
-    private void pauseclick(){
-        flag=false;
-    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        bpmtext=(TextView)findViewById(R.id.bpmtext);
-
-        bpmtext.setText("BPM");
-
-        play=(Button) findViewById(R.id.play);
-        play.setText("Play");
-        play.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-                if (paused) {
-
-                    play.setText("Pause");
-                    paused=false;
-
-
-                }
-                else {
-
-                    play.setText("Play");
-                    paused=true;
-                }
-
-            }
-        });
-
-
-        record=(Button) findViewById(R.id.record);
-        record.setText("Record");
-        record.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-                if (recorded) {
-
-                    record.setText("Stop");
-                    recorded=false;
-
-
-                }
-                else {
-
-                    record.setText("Record");
-                    recorded=true;
-                }
-
-            }
-        });
-
-        save=(Button) findViewById(R.id.save);
-        save.setText("Save");
-
-        save.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-                Toast.makeText(MainActivity.this, "Recording Saved",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        bpm =(SeekBar)findViewById(R.id.bpm);
-
-        bpm.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                bpmtext.setText(progress + "BPM");
-
-            }
-
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // TODO Auto-generated method stub
-            }
-
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        minimenu = (Spinner) findViewById(R.id.optionspinner);
-
-        //minimenu.setOnItemSelectedListener(this);
-
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,menu);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        minimenu.setAdapter(aa);
-
-
+        //Making AudioTracks
         int minBufferSize = AudioTrack.getMinBufferSize(44100, AudioFormat.CHANNEL_OUT_STEREO, AudioFormat.ENCODING_PCM_16BIT);
         track[0] = new AudioTrack.Builder()
                 .setAudioAttributes(new AudioAttributes.Builder()
@@ -197,44 +133,68 @@ public class MainActivity extends AppCompatActivity {
                 .setTransferMode(AudioTrack.MODE_STREAM)
                 .setBufferSizeInBytes(minBufferSize)
                 .build();
-//        setResource(0, R.raw.guitarc4);
-//        setResource(1, R.raw.guitarc3);
-        Button pause=(Button) findViewById(R.id.button2);
-        pause.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
 
-                pauseclick();
-            }
-        });
-        Button play = (Button) findViewById(R.id.play);
+        //XML Connections
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        bpmtext=(TextView)findViewById(R.id.bpmtext);
+
+        bpmtext.setText("BPM");
+
+        play = (Button) findViewById(R.id.play);
+
         play.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                while (flag) {
-                    Runnable r = new Runnable() {
-                        @Override
-                        public void run() {
-                            doStuff(0, R.raw.guitarc4);
-                        }
-                    };
-                    Thread t1 = new Thread(r);
-                    t1.start();
-
-                    long futureTime = System.currentTimeMillis() + 500;
-                    while (System.currentTimeMillis() < futureTime) {
-                        synchronized (this) {
-                            try {
-                                wait(futureTime - System.currentTimeMillis());
-
-                            } catch (Exception e) {
-                            }
-                        }
-                    }
-                    doStuff(1, R.raw.guitarc3);
-                }
+            public void onClick(View v) {
+                play();
+//                if (isPlaying) {
+//                    play.setText("Play");
+//                    isPlaying = false;
+//                    pause();
+//                }
+//                else {
+//                    play.setText("Pause");
+//                    Log.d(getClass().getName(), "Set text to Pause: " );
+//                    isPlaying = true;
+//                    play();
+//                    Log.d(getClass().getName(), "Play is done " );
+//                }
             }
         });
+        record=(Button) findViewById(R.id.record);
+        record.setText("Record");
+        record.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                pause();
+            }
+        });
+
+        save=(Button) findViewById(R.id.save);
+        save.setText("Save");
+        save.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Recording Saved",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bpm =(SeekBar)findViewById(R.id.bpm);
+        bpm.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                bpmtext.setText(progress + "BPM");
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
+        minimenu = (Spinner) findViewById(R.id.optionspinner);
+        //minimenu.setOnItemSelectedListener(this);
+        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,menu);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        minimenu.setAdapter(aa);
     }
 
 
@@ -264,11 +224,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.navmenu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+
 
 }
 
